904c85ed00931dedaeddf31c61e6d7c80cc4ba16
walkgs <walkingfgxxt@gmail.com>
2024-12-06 18:20:07 -0300

    first commit

---
+++ src/main/java/com/completize/core/global/Applicable.java
@@ -0,0 +1,14 @@
+package com.completize.core.global;
+
+import java.util.function.Consumer;
+
+public interface Applicable<T extends Applicable> {
+
+    @SuppressWarnings("unchecked")
+    default T apply(Consumer<T> apply) {
+        final T object = (T) this;
+        apply.accept(object);
+        return object;
+    }
+
+}
---
+++ src/main/java/com/completize/core/global/Primitive.java
@@ -0,0 +1,22 @@
+package com.completize.core.global;
+
+import java.util.Map;
+
+public class Primitive {
+
+    private static final Map<Class<?>, Class<?>> PRIMITIVE_MAP = Map.of(
+            boolean.class, Boolean.class,
+            byte.class, Byte.class, char.class,
+            Character.class, double.class,
+            Double.class, float.class,
+            Float.class, int.class, Integer.class,
+            long.class, Long.class,
+            short.class, Short.class
+    );
+
+    public static Class<?> translate(Class<?> type) {
+        final Class<?> conversion = PRIMITIVE_MAP.get(type);
+        return conversion != null ? conversion : type;
+    }
+
+}
